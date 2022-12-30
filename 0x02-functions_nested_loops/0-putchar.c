31c2435cda8b200f0f038f779c175e28d02e0216
Emem09 <emem.mbre2@gmail.com>
2022-12-29 19:33:36 -0800

    a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a function that prints _puchar followed by a new line
+ * Return: 0 Always on Success
+ */
+
+int main(void)
+
+{
+	printf("_putchar\n");
+	return (0);
+
+}
